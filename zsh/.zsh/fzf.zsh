524024929dced8fe6f6f8c0c27a988ba995d83b8
tbosmans <tom.94.bosmans@gmail.com>
2024-07-18 20:57:01 +0200

    fixup(zsh): correctly set fzf colors

--- zsh/.zsh/fzf.zsh
+++ zsh/.zsh/fzf.zsh
@@ -2,12 +2,10 @@
 
 [ -f /usr/share/fzf/key-bindings.zsh ] && source /usr/share/fzf/key-bindings.zsh
 
-local colors="--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
 --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
 --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
-
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
-$colors
 
 if 'zmodload' 'zsh/parameter' 2>'/dev/null' && (( ${+options} )); then
   __fzf_key_bindings_options="options=(${(j: :)${(kv)options[@]}})"
