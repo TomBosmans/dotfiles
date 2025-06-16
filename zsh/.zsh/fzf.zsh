db695fdd8d711c29328f21183b5abef23606b68b
tbosmans <tom.94.bosmans@gmail.com>
2025-06-16 21:46:50 +0200

    some fixups

--- zsh/.zsh/fzf.zsh
+++ zsh/.zsh/fzf.zsh
@@ -5,4 +5,8 @@ export FZF_DEFAULT_OPTS=" \
 --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
 --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
 
+zstyle ':fzf-tab:*' fzf-flags \
+  --color=bg:#24273a,bg+:#363a4f,fg:#cad3f5,fg+:#cad3f5,hl:#ed8796,hl+:#ed8796,spinner:#f4dbd6,pointer:#f4dbd6,marker:#f4dbd6,prompt:#c6a0f6,header:#ed8796,info:#c6a0f6 \
+  --bind=tab:accept
+
 source <(fzf --zsh)
