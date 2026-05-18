ab1a90295a8b5628c9be80c94343d2c2969d5151
tbosmans <tom.94.bosmans@gmail.com>
2026-05-18 10:07:55 +0200

    feat(nvim): add markdown-plus.nvim plugin

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,2 +1,7 @@
-vim.pack.add({ "https://github.com/MeanderingProgrammer/render-markdown.nvim" })
+vim.pack.add({
+  "https://github.com/MeanderingProgrammer/render-markdown.nvim",
+  "https://github.com/YousefHadder/markdown-plus.nvim"
+})
+
 require('render-markdown').setup({})
+require("markdown-plus").setup({})
